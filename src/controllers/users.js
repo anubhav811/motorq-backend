4fa2ecb72d3059dc4daafc9db19fd5fc9372621e
Anubhav Chachra <chachraanubhav@gmail.com>
2022-08-18 16:57:26 +0530

    updated routes

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,53 +1,81 @@
-const User=require('../models/users');
+const User = require("../models/users");
+const Event = require("../models/events");
 
-const home=(req,res)=>{
-    try{
-        res.status(200).send({
-            message:"Welcome to the users page"
-        });
-    }catch(error){
-        res.status(400).send(error);
-    }
-}
+const home = (req, res) => {
+  try {
+    res.status(200).send({
+      message: "Welcome to the users page",
+    });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
 
-const register=async(req,res)=>{
-    const user=new User(req.body);
-    try{
-        const token=await user.generateAuthToken()
-        await user.save();
-        res.status(201).send(user)
-     }catch(error){
-        console.log(error);
-        res.status(400).send(error)
-    }
-}
+const register = async (req, res) => {
+  const user = new User(req.body);
+  try {
+    const token = await user.generateAuthToken();
+    await user.save();
+    res.status(201).send(user);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
+};
 
-const login =async(req,res)=>{
-    try{
-        const user=await User.findByCredentials(req.body.username,req.body.password)
-        await user.generateAuthToken()
-        res.status(200).send(user)
-    }catch(error){
-        console.log(error);
-        res.status(400).send(error)
-    }    
-}
+const login = async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.username,
+      req.body.password
+    );
+    await user.generateAuthToken();
+    res.status(200).send(user);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
+};
 
-const logout=async(req,res)=>{
-    try{
-        req.user.token="";
-        await req.user.save()
-        res.status(200).send({
-            message:"Logout Successful"
-        })
-    }catch(error){
-        res.status(400).send(error)
-    }
-}
+const registerEvent = async (req, res) => {
+  try {
+    const user = await User.findById(req.body.userId);
+    user.events.push(req.body.eventId);
+    await user.save();
+    res.status(200).send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
 
-module.exports={
-    home,
-    register,
-    login,
-    logout
-}
+const getEventsList = async (req, res) => {
+  try {
+    const user = await User.findById(req.body.userId);
+    const events = await Event.find({ _id: { $in: user.events } });
+    res.status(200).send(events);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
+};
+
+const logout = async (req, res) => {
+  try {
+    req.user.token = "";
+    await req.user.save();
+    res.status(200).send({
+      message: "Logout Successful",
+    });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+module.exports = {
+  home,
+  register,
+  login,
+  registerEvent,
+  getEventsList,
+  logout,
+};
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,11 +1,20 @@
-const express=require("express");
-const {home,register,login,logout}=require("../controllers/users");
-const auth=require("../middleware/user");
-const router=new express.Router();
+const express = require("express");
+const {
+  home,
+  register,
+  login,
+  logout,
+  registerEvent,
+  getEventsList,
+} = require("../controllers/users");
+const auth = require("../middleware/user");
+const router = new express.Router();
 
-router.get("/",home);
-router.post("/register",register);
-router.post("/login",login);
-router.get("/logout",auth,logout);
+router.get("/", home);
+router.post("/register", register);
+router.post("/login", login);
+router.post("/registerEvent", auth, registerEvent);
+router.post("/events", auth, getEventsList);
+router.get("/logout", auth, logout);
 
-module.exports=router;
+module.exports = router;
